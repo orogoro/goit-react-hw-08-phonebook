2b3816895bf00c63c933c97377b32f90a2f68023
Yurii Dvornichenko <raen98@live.ru>
2022-06-02 18:52:48 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,6 +17,20 @@ export default class App extends Component {
     filter: '',
   };
 
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
   addContact = ({ name, number }) => {
     const findName = this.state.contacts
       .map(contact => contact.name)
