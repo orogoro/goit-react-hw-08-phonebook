51a81561d634f65c27a0a072a6f324ff17154a38
Yurii Dvornichenko <raen98@live.ru>
2022-07-14 20:59:07 +0300

    create homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,3 +1,14 @@
+import styles from './HomePage.module.css';
+
 export default function HomePage() {
-  return;
+  return (
+    <main className={styles.main}>
+      <div className={styles.container_title}>
+        <h1>
+          Welcome to <span className={styles.span}>Phonebook</span>
+        </h1>
+        <p className={styles.p}>Next generation</p>
+      </div>
+    </main>
+  );
 }
